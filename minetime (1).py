a036a41eeb854ec271accb1052e26940be6b1fbf
xursinerx <ursatab@gmail.com>
2024-11-26 19:15:29 +0200

    Update minetime (1).py

--- minetime (1).py
+++ minetime (1).py
@@ -50,7 +50,7 @@ def main():
     Loads the game graphics, creates a game window and sets a draw handler for it.
     """
     sweeperlib.load_sprites(r"C:\Users\msinu\OneDrive\Documents\GitHub\Elem_programming\sprites")
-    #C:\Users\msinu\OneDrive\Documents\GitHub\Elem_programming\sprites
+    #r"C:\Users\msinu\OneDrive\Documents\GitHub\Elem_programming\sprites"
     #"/home/ursa/Documents/coding/sprites"
     sweeperlib.create_window(600, 400)
 
